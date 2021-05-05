ec0567381efd5db978974db3a6fd9f49a19514b5
João Paulo <jp_cbc@hotmail.com>
2021-05-04 21:05:47 -0300

    Adicionando componenetes estáticos

---
+++ frontend/src/components/footer/index.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/jopaulo666/projeto-sds3" target="_blank" rel="noreferrer">João Paulo da Mata Mendes</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+export default Footer;
