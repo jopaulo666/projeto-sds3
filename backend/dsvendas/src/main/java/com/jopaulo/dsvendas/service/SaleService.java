56ce5a92117745664a3573cb5823542fe98b66ec
João Paulo <jp_cbc@hotmail.com>
2021-05-06 19:38:06 -0300

    Buscas agrupadas (GROUP BY)

--- backend/dsvendas/src/main/java/com/jopaulo/dsvendas/service/SaleService.java
+++ backend/dsvendas/src/main/java/com/jopaulo/dsvendas/service/SaleService.java
@@ -1,5 +1,7 @@
 package com.jopaulo.dsvendas.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.jopaulo.dsvendas.dto.SaleDTO;
+import com.jopaulo.dsvendas.dto.SaleSuccessDTO;
+import com.jopaulo.dsvendas.dto.SaleSumDTO;
 import com.jopaulo.dsvendas.entities.Sale;
 import com.jopaulo.dsvendas.repository.SaleRepository;
 import com.jopaulo.dsvendas.repository.SellerRepository;
@@ -16,14 +20,24 @@ public class SaleService {
 
 	@Autowired
 	private SaleRepository repository;
-	
+
 	@Autowired
 	private SellerRepository sellerRepository;
-	
+
 	@Transactional(readOnly = true) // garante que toda operação do banco seja resolvido nesse método
-	public Page<SaleDTO> findAll(Pageable pageable){
-		sellerRepository.findAll(); // deixa na memória todos os vendedores
+	public Page<SaleDTO> findAll(Pageable pageable) {
+		sellerRepository.findAll(); // deixa na mem
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
 	}
 }
